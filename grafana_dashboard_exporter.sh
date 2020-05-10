b277a8aa79de8a91af3c63408a7b22e03792dbf8
Adam Eaton <adameaton76@gmail.com>
2020-05-10 20:21:07 +0100

    Automatically commited by grafana_dashboard_exporter.sh

--- grafana_dashboard_exporter.sh
+++ grafana_dashboard_exporter.sh
@@ -11,7 +11,7 @@ fi
 mkdir "$EXPORT_DIR"
 
 for dash in $(curl -sSL -k -H "Authorization: Bearer $KEY" $HOST/api/search\?query\=\& | jq '.' |grep -i uri|awk -F '"uri": "' '{ print $2 }'|awk -F '"' '{print $1 }'); do
-    curl -sSL -k -H "Authorization: Bearer ${KEY}" "${HOST}/api/dashboards/${dash}" > "$EXPORT_DIR"/$(echo ${dash}|sed 's,db/,,g').json
+    curl -sSL -k -H "Authorization: Bearer ${KEY}" "${HOST}/api/dashboards/${dash}" | jq '.dashboard.id = null' | jq '.dashboard' > "$EXPORT_DIR"/$(echo ${dash}|sed 's,db/,,g').json
 done
 
 for file in $(ls "$EXPORT_DIR"); do
